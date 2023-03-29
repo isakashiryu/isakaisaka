229cefd1a08ad1b17789ad6177d3086033af9c48
seino <skim07a19yki@icloud.com>
2023-03-30 02:02:19 +0900

    add change_domain

---
+++ change_domain.py
@@ -0,0 +1,9 @@
+def change_domain(email, domain):
+    return '@'.join([email.split('@')[0], domain])
+
+# 使用例
+old_email = 'seino.kaimu@icloud.com.com'
+new_domain = 'newdomain.com'
+
+new_email = change_domain(old_email, new_domain)
+print(new_email)
