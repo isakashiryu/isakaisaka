ed882aba2663764982190882177e1762497436ca
isakashiryu <siryu0641@gnmail.com>
2023-03-30 01:37:03 +0900

    commit

--- python_second.py
+++
@@ -1,4 +0,0 @@
-# emailアドレス email とドメイン名 domain を引数に取って、email のドメイン名を domain に置き換える関数 change_domain(email, domain) を作成してください。 なお、emailアドレスのドメイン名とは、 '@' で区切られた右側の部分を意味します。
-# 次のセルの ... のところを書き換えて change_domain(email, domain) を完成させてください。
-def change_domain(email, domain):
-    return '@'.join([email.split('@')[0], domain])
