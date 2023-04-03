3d18d53df3011d517a02f7864f8b06587a84e2b4
isakashiryu <siryu0641@gnmail.com>
2023-04-03 11:49:40 +0900

    first commit

---
+++ tokyoU_3-2.py
@@ -0,0 +1,72 @@
+# 練習１
+def reverse_lookup2(dic1):
+    dic2 = {}
+    for key,value in dic1.items():
+        dic2[value] = key
+    return dic2    
+
+# 練習２
+def sum_n(x,y):
+    s = 0
+    for n in range(x,y+1):
+        s = s + n
+    return s    
+
+# 練習３
+def construct_list(int_size):
+    list1 = []
+    for list_value in range(int_size):
+        list1 = list1 + [list_value]
+    return list1    
+
+# 練習4
+def sum_lists(list1):
+    sum = 0
+    for list2 in list1:
+        for n in list2:
+            sum = sum + n
+    return sum        
+         
+# 練習5
+def sum_matrix(list1,list2):
+    list3 = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(3):
+        for j in range(3):
+            list3[i][j] += list1[i][j] + list2[i][j]
+    return list3       
+
+# 練習6
+def simple_match(str1, str2):
+    for i in range(len(str1)-len(str2)+1):
+        j = 0
+        while j < len(str2) and str1[i+j] == str2[j]:  #str1とstr2が一致している限りループ（ただし、jがstr2の長さ以上にならないようにする）#この条件がないと…？
+            j += 1
+        if j == len(str2):  #str2の最後まで一致しているとこの条件が成立
+            return i
+    return -1
+    
+#練習７
+# from time import sleep
+# x = 0
+# while True:
+#     if x >= 10:
+#         break
+#     print('Yeah!')
+#     sleep(1)
+#     x += 1
+
+# 練習8
+def collect_engwords(str_engsentences):
+    list_punctuation = ['.', ',', ':', ';', '!', '?']
+    for j in range(len(list_punctuation)):  #list_punctuationの中の文字列（この場合、句読点）を空文字列に置換する
+        str_engsentences = str_engsentences.replace(list_punctuation[j], '')
+    list_str1 = str_engsentences.split(' ')
+    list_str2 = []
+    for str_engwords in list_str1:
+        if len(str_engwords) >= 3:
+            list_str2 += str_engwords
+    return list_str2
+
+      
+
+# 練習9
