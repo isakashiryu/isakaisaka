c66f67f59c033bfd33faccc64a778dcd701f4974
isakashiryu <siryu0641@gnmail.com>
2023-04-12 10:06:42 +0900

    commit

---
+++ tokyoU_5-3.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('/Users/michel/anaconda3/lib/python3.10/site-packages')
+import numpy as np
+
+def arange_square_matrix(n):
+    return np.array([np.arange(i, n+i) for i in range(n)])
+
+                                     # ↑for文にはこんな使い方もある
+
+
+
+      
+
+
